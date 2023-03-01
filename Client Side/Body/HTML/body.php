bbbe87ed2a42b8ff0c7a7fed7882b2d04e0035bf
Sunhor <sunhorly@gmail.com>
2023-03-01 15:24:38 +0700

    body

---
+++ Client Side/Body/HTML/body.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>body-card</title>
+    <link rel="stylesheet" href="../CSS/body.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+<body>
+    <div class="body">
+        <div class="body-container">
+            <img src="../img/image1.jpeg" alt="">
+        </div>
+        <div class="logo">
+            <img src="../img/Logo.png" alt="">
+        </div>
+    </div>
+    <div class="title">
+        <h2>PRODUCT-STORE</h2>
+        <h4>Here you can check out our all products!</h4>
+    </div>
+    <div class="main-card">
+        <div class="card-product">
+            <div class="box">
+                 <img src="../img/krorma5.jpg" alt="">
+                 <h5>Krorma</h5>
+            </div>
+            <div class="box">
+                 <img src="../img/krorma1.jpg" alt="">
+                 <h5>Krorma</h5>
+            </div>
+            <div class="box">
+                <img src="../img/krorma2.jpg" alt="">
+                <h5>Krorma</h5>
+            </div>
+            <div class="box">
+                <img src="../img/krorma3.jpg" alt="">
+                <h5>Krorma</h5>
+           </div>
+        </div>
+    </div>
+    <!-- start krorma products -->
+    <h2>KRORMA PRODUCT</h2>
+    <div class="main-card">
+      <div class="main-box">
+          <div class="box-product">
+             <img src="../img/krorma1.jpg" alt="">
+             <div class="box-detail">
+                <h3>Krorma Khmer</h3>
+                <h3>20$</h3>
+                <h3><a href="">Detail</a></h3>
+             </div>
+          </div>
+          <div class="box-product">
+             <img src="../img/krorma2.jpg" alt="">
+             <div class="box-detail">
+                <h3>Krorma Khmer</h3>
+                <h3>20$</h3>
+                <h3><a href="">Detail</a></h3>
+             </div>
+          </div>
+          <div class="box-product">
+             <img src="../img/krorma3.jpg" alt="">
+             <div class="box-detail">
+                <h3>Krorma Khmer</h3>
+                <h3>20$</h3>
+                <h3><a href="">Detail</a></h3>
+             </div>
+          </div>
+          <div class="box-product">
+             <img src="../img/krorma5.jpg" alt="">
+             <div class="box-detail">
+                <h3>Krorma Khmer</h3>
+                <h3>20$</h3>
+                <h3><a href="">Detail</a></h3>
+             </div>
+          </div>
+      </div>
+    </div>
+    <div class="main-card">
+      <div class="main-box2">
+          <div class="box-product">
+             <img src="../img/krorma1.jpg" alt="">
+             <div class="box-detail">
+                <h3>Krorma Khmer</h3>
+                <h3>20$</h3>
+                <h3><a href="">Detail</a></h3>
+             </div>
+          </div>
+          <div class="box-product">
+             <img src="../img/krorma2.jpg" alt="">
+             <div class="box-detail">
+                <h3>Krorma Khmer</h3>
+                <h3>20$</h3>
+                <h3><a href="">Detail</a></h3>
+             </div>
+          </div>
+          <div class="box-product">
+             <img src="../img/krorma3.jpg" alt="">
+             <div class="box-detail">
+                <h3>Krorma Khmer</h3>
+                <h3>20$</h3>
+                <h3><a href="">Detail</a></h3>
+             </div>
+          </div>
+          <div class="box-product">
+             <img src="../img/krorma5.jpg" alt="">
+             <div class="box-detail">
+                <h3>Krorma Khmer</h3>
+                <h3>20$</h3>
+                <h3><a href="">Detail</a></h3>
+             </div>
+          </div>
+      </div>
+    </div>
+     <!-- end krorma products -->
+      <!-- start karom products -->
+    <div class="kaorm"><h2>KAORM PRODUCT</h2></div>
+    <div class="main-card">
+      <div class="main-box">
+          <div class="box-product">
+             <img src="../img/kraom1.jpg" alt="">
+             <div class="box-detail">
+                <h3>Kaorm Khmer</h3>
+                <h3>10.5$</h3>
+                <h3><a href="">Detail</a></h3>
+             </div>
+          </div>
+          <div class="box-product">
+             <img src="../img/kraom2.jpg" alt="">
+             <div class="box-detail">
+                <h3>Karom Khmer</h3>
+                <h3>11.35$</h3>
+                <h3><a href="">Detail</a></h3>
+             </div>
+          </div>
+          <div class="box-product">
+             <img src="../img/kraom3.jpg" alt="">
+             <div class="box-detail">
+                <h3>Karom Khmer</h3>
+                <h3>8.5$</h3>
+                <h3><a href="">Detail</a></h3>
+             </div>
+          </div>
+          <div class="box-product">
+             <img src="../img/kraom4.jpeg" alt="">
+             <div class="box-detail">
+                <h3>Karom Khmer</h3>
+                <h3>12.99$</h3>
+                <h3><a href="">Detail</a></h3>
+             </div>
+          </div>
+      </div>
+    </div>
+    <div class="main-card">
+      <div class="main-box2k">
+          <div class="box-product">
+             <img src="../img/kraom1.jpg" alt="">
+             <div class="box-detail">
+                <h3>Karom Khmer</h3>
+                <h3>0.5$</h3>
+                <h3><a href="">Detail</a></h3>
+             </div>
+          </div>
+          <div class="box-product">
+             <img src="../img/kraom2.jpg" alt="">
+             <div class="box-detail">
+                <h3>Karom Khmer</h3>
+                <h3>2.5$</h3>
+                <h3><a href="">Detail</a></h3>
+             </div>
+          </div>
+          <div class="box-product">
+             <img src="../img/kraom3.jpg" alt="">
+             <div class="box-detail">
+                <h3>Karom Khmer</h3>
+                <h3>99$</h3>
+                <h3><a href="">Detail</a></h3>
+             </div>
+          </div>
+          <div class="box-product">
+             <img src="../img/kraom4.jpeg" alt="">
+             <div class="box-detail">
+                <h3>Karom Khmer</h3>
+                <h3>20$</h3>
+                <h3><a href="">Detail</a></h3>
+             </div>
+          </div>
+      </div>
+    </div>
+     <!-- end krorma products -->
+</body>
+</html>
