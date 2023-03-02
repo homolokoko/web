c1d81b3a5081a4169696f25f045171b1529ec9b6
Tong <hengtong.sou@gmail.com>
2023-03-02 19:21:20 +0700

    on users account

---
+++ Client Side/Users_Account/sign-in/sign-in.php
@@ -0,0 +1,28 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- <link rel="stylesheet" href="../CSS/style.css"> -->
+    <link rel="stylesheet" href="sign-in.css">
+</head>
+<body>
+    <form action="" class="frm_sign-in">
+
+        <div class="sign_in">
+            <div class="title">
+                <h3>WELCOME BACK</h3>
+                <p>Not registered?<a href="" style="color:black;font-weight:500;text-decoration:none;">Sing up</a></p>
+            </div>
+            <input type="text" name="name" id="name" placeholder="Name">
+            <input type="password" name="password" id="password" placeholder="Password">
+            <input type="submit" id="signin" value="Sign In">
+        </div>
+        <div class="image">
+            <img src="../../img/twins.jpg" alt="">
+        </div>
+        
+    </form>
+</body>
+</html>
---
+++ Client Side/Users_Account/sign-up/sign-up.php
@@ -0,0 +1,30 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- <link rel="stylesheet" href="../CSS/style.css"> -->
+    <link rel="stylesheet" href="sign-up.css">
+</head>
+<body>
+    <form action="" class="frm_sign-up">
+
+        <div class="image">
+            <img src="../../img/twins.jpg" alt="">
+        </div>
+        <div class="sign_up">
+            <div class="title">
+                <h3>GET STARTED</h3>
+                <p style="font-size:10pt;">Already have an account?<a href="" style="color:black;font-weight:500;text-decoration:none;">Sing in</a></p>
+            </div>
+            <input type="text" name="name" id="name" placeholder="Name">
+            <input type="email" id="email" name="email" placeholder="Email">
+            <input type="password" name="password" id="password" placeholder="Password">
+            <input type="submit" id="signin" value="Sign Up">
+        </div>
+        
+        
+    </form>
+</body>
+</html>
