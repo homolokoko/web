3186c7ac151c73b523deb9316c4bd49587c8d977
Entrakboth <roeunentrakboth889@gmail.com>
2023-03-08 20:35:31 +0700

    update table

---
+++ Admin Side/App/Body/HTML/Product_table.php
@@ -0,0 +1,29 @@
+<head>
+    <link rel="stylesheet" href="../CSS/Product_table.css">
+</head>
+<body>
+<table class="content-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Points</th>
+            <th>Team</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Domenic</td>
+            <td>88,110</td>
+            <td>dcode</td>
+          </tr>
+          <tr class="active-row">
+            <td>2</td>
+            <td>Sally</td>
+            <td>72,400</td>
+            <td>Students</td>
+          </tr>
+        </tbody>
+      </table>
+</body>
